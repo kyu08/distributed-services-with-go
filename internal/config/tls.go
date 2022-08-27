92872c12bbb34bdbc3e4b80d5aa309af7d916c2d
kyu08 <kyu08@users.noreply.github.com>
2022-08-27 17:23:48 +0900

    add comment

--- internal/config/tls.go
+++ internal/config/tls.go
@@ -7,10 +7,13 @@ import (
 	"os"
 )
 
+// configをsetupしてポインタを返すくん
 func SetupTLSConfig(cfg TLSConfig) (*tls.Config, error) {
 	var err error
 	tlsConfig := &tls.Config{MinVersion: tls.VersionTLS13}
 	if cfg.CertFile != "" && cfg.KeyFile != "" {
+		// NOTE: 「クライアントの*tls.Config には、RootCAs と Certificates を設定することで、サーバ の証明書を検証し、サーバがクライアントの証明書を検証できるように設定されます。」
+		// ってあるけどここの判定 cfg.Server じゃだめなんだろうか
 		tlsConfig.Certificates = make([]tls.Certificate, 1)
 		tlsConfig.Certificates[0], err = tls.LoadX509KeyPair(
 			cfg.CertFile,
@@ -20,11 +23,13 @@ func SetupTLSConfig(cfg TLSConfig) (*tls.Config, error) {
 			return nil, err
 		}
 	}
+
 	if cfg.CAFile != "" {
 		b, err := os.ReadFile(cfg.CAFile)
 		if err != nil {
 			return nil, err
 		}
+
 		ca := x509.NewCertPool()
 		ok := ca.AppendCertsFromPEM([]byte(b))
 		if !ok {
@@ -33,14 +38,20 @@ func SetupTLSConfig(cfg TLSConfig) (*tls.Config, error) {
 				cfg.CAFile,
 			)
 		}
+
 		if cfg.Server {
+			// サーバーの証明書
+
 			tlsConfig.ClientCAs = ca
 			tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
 		} else {
+			// クライアントの証明書
+
 			tlsConfig.RootCAs = ca
 		}
 		tlsConfig.ServerName = cfg.ServerAddress
 	}
+
 	return tlsConfig, nil
 }
 
